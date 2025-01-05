58f5b1b3770306fab744e41aa2f8afaf4265a8ce
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-01-05 17:47:41 +0900

    move player

---
+++ game_scene/maze/player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+const TILE_SIZE=32
+@export var speed = TILE_SIZE
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 1
+		
+		
+		
+	#velocity = velocity.normalized() * speed
+	#position+=velocity*delta
+	
+	var new_position=position+velocity*TILE_SIZE
+	new_position.x=round(new_position.x/TILE_SIZE)*TILE_SIZE
+	new_position.y=round(new_position.y/TILE_SIZE)*TILE_SIZE
+	
+	position=new_position
+	
