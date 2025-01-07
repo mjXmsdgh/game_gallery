7a4173d45889cff9602ae5178839665755aeafb7
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-01-07 21:16:26 +0900

    とりあえず

--- game_scene/maze/player.gd
+++ game_scene/maze/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 const TILE_SIZE=32
 @export var speed = TILE_SIZE
+@onready var sound_player=get_node("/root/Maze/AudioStreamPlayer2D")
+@onready var sound_player_go=get_node("/root/Maze/AudioStreamPlayer2D2")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +21,9 @@ func _process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.y -= 1
+		
+	if (velocity.x==0)and(velocity.y==0):
+		return
 	
 	# 移動量
 	var move_value=velocity*TILE_SIZE
@@ -26,10 +31,16 @@ func _process(delta: float) -> void:
 	# 新しい位置
 	var new_position=position+move_value
 	
-	var collision=move_and_collide(move_value)
 	
-	if collision :
-		print("collision detected")
+	#var collision=move_and_collide(move_value)
+	#if collision :
+	#	print("collision detected")
+	#	sound_player.play_sound()
+	#	return
+	
+	if test_move(global_transform,move_value):
+		sound_player.play_sound()
+		return
 	
 	# 丸める
 	new_position.x=round(new_position.x/TILE_SIZE)*TILE_SIZE
@@ -37,4 +48,5 @@ func _process(delta: float) -> void:
 	
 	# 移動
 	position=new_position
+	sound_player_go.play_sound()
 	
