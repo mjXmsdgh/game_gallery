8f7bffd5efe95144f8fe748454c2b63045c31bab
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-01-11 20:13:14 +0900

    add timer

---
+++ game_scene/maze/maze.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var time_label=$timer
+var start_time=0
+var elapsed_time=0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_time=Time.get_ticks_msec()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	elapsed_time=(Time.get_ticks_msec()-start_time)/1000.0
+
+	# 時分秒
+	var hours=int(elapsed_time/3600)
+	var minutes=int(fmod(elapsed_time,3600)/60.0)
+	var seconds=int(fmod(elapsed_time,3600))
+	
+	# 更新
+	time_label.text="Time: %02d:%02d:%02d" % [hours,minutes,seconds]
