8fa04ea2f9956e0fa2a127e1ba274c8a8f6655d3
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-01-23 20:25:42 +0900

    move player

--- game_scene/shooting/2_main/shooting_player.gd
+++ game_scene/shooting/2_main/shooting_player.gd
@@ -1,45 +1,33 @@
 extends CharacterBody2D
 
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
 const TILE_SIZE=32
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#var velocity = Vector2.ZERO # The player's movement vector.
+	#
+	## キー入力
+	#if Input.is_action_just_pressed("ui_right"):
+		#velocity.x += 1
+	#if Input.is_action_just_pressed("ui_left"):
+		#velocity.x -= 1
+	#if Input.is_action_just_pressed("ui_down"):
+		#velocity.y += 1
+	#if Input.is_action_just_pressed("ui_up"):
+		#velocity.y -= 1
+		#
+	## 入力なし
+	#if (velocity.x==0)and(velocity.y==0):
+		#return
+	#
+	## 移動処理
+	#move(velocity);
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	#move_and_slide()
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
+func move(velocity: Vector2) -> void:
 	
-	# キー入力
-	if Input.is_action_just_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_just_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_just_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y -= 1
-		
-	# 入力なし
+	# 移動しない
 	if (velocity.x==0)and(velocity.y==0):
 		return
 	
@@ -59,3 +47,23 @@ func _process(delta: float) -> void:
 	
 	# 移動
 	position=new_position
+
+
+func _on_button_up_pressed() -> void:
+	var velocity=Vector2(0,-1)
+	self.move(velocity)
+
+
+func _on_button_down_pressed() -> void:
+	var velocity=Vector2(0,1)
+	self.move(velocity)
+
+
+func _on_button_left_pressed() -> void:
+	var velocity=Vector2(-1,0)
+	self.move(velocity)
+
+
+func _on_button_right_pressed() -> void:
+	var velocity=Vector2(+1,0)
+	self.move(velocity)
