84709917707193a0bf7abe32baa53f22fed52b3d
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-01-31 20:23:34 +0900

    auto scroll

--- game_scene/shooting/2_main/main.gd
+++ game_scene/shooting/2_main/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var time=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +12,15 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ui_up"):
 		$player._on_button_up_pressed()
-		
-	if Input.is_action_just_pressed("ui_down"):
+	elif Input.is_action_just_pressed("ui_down"):
 		$player._on_button_down_pressed()
-		
-	if Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_just_pressed("ui_left"):
 		$player._on_button_left_pressed()
-		
-	if Input.is_action_just_pressed("ui_right"):
+	elif Input.is_action_just_pressed("ui_right"):
 		$player._on_button_right_pressed()
+		
+	time=time+delta
+	
+	if time>=1.0:
+		$player._on_button_up_pressed()
+		time=0
