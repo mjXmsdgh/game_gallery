fda598d868623633364b77164b16c376b1535481
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-10 13:57:53 +0900

    change main script

--- 1_main/button_shooting.gd
+++ 1_main/button_shooting.gd
@@ -1,5 +1,6 @@
 extends Button
 
+@export var shootingScene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +12,13 @@ func _process(delta: float) -> void:
 	pass
 	
 func _pressed():
-	get_tree().change_scene_to_file("res://game_scene/shooting/1_start/start.tscn")
 	
+	# mazeを実体化
+	var shooting_instance:Node=shootingScene.instantiate()
+
+	# 子ノードに追加
+	var root:Node=get_tree().get_root()
+	root.add_child(shooting_instance)
+
+	# 終わったら開放
+	get_tree().current_scene.queue_free()	
--- 1_main/button_to_maze.gd
+++ 1_main/button_to_maze.gd
@@ -1,5 +1,6 @@
 extends Button
 
+@export var MazeScene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +10,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
+
 func _pressed():
-	#get_tree().change_scene_to_file("res://game_scene/maze.tscn")
-	get_tree().change_scene_to_file("res://game_scene/maze/1_start/start.tscn")
-	
+
+	# mazeを実体化
+	var maze_instance:Node=MazeScene.instantiate()
+
+	# 子ノードに追加
+	var root:Node=get_tree().get_root()
+	root.add_child(maze_instance)
+
+	# 終わったら開放
+	get_tree().current_scene.queue_free()
