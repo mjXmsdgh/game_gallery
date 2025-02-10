6b25fcc404ed4996b96b0b6931c332a6832b95a9
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-10 14:03:10 +0900

    change maze main

--- 2_game_scene/maze/1_start/button_start.gd
+++ 2_game_scene/maze/1_start/button_start.gd
@@ -1,5 +1,6 @@
 extends Button
 
+@export var main_scene:PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,4 +17,13 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://game_scene/maze/2_main/maze.tscn")
+
+	# mazeを実体化
+	var main_instance:Node=main_scene.instantiate()
+
+	# 子ノードに追加
+	var root:Node=get_tree().get_root()
+	root.add_child(main_instance)
+
+	# 終わったら開放
+	get_tree().current_scene.queue_free()	
