e3321fdeccd60c6d046c5dc8c19eea63b7ebe301
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-13 20:06:02 +0900

    add player

---
+++ 2_game_scene/maze_new/script/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+const TILE_SIZE=32
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+@onready var sound_go=$sound_go
+@onready var sound_stop=$sound_stop
+
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	# キー入力
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 1
+		
+	# 入力なし
+	if (velocity.x==0)and(velocity.y==0):
+		return
+	
+	# 移動量
+	var move_value=velocity*TILE_SIZE
+	
+	# 新しい位置
+	var new_position=position+move_value
+	
+	# 移動できるかチェック
+	if test_move(global_transform,move_value):
+		sound_stop.play_sound()
+		return
+	
+	# 丸める
+	new_position.x=round(new_position.x/TILE_SIZE)*TILE_SIZE
+	new_position.y=round(new_position.y/TILE_SIZE)*TILE_SIZE
+	
+	# 移動
+	position=new_position
+	sound_go.play_sound()	
