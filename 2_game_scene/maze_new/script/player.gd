e032e70735fde34bc8bb7f60f31405ec1ccf6ab8
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-15 20:46:02 +0900

    当たり判定の訂正

--- 2_game_scene/maze_new/script/player.gd
+++ 2_game_scene/maze_new/script/player.gd
@@ -18,26 +18,25 @@ func _process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.y -= 1
-		
+
 	# 入力なし
-	if (velocity.x==0)and(velocity.y==0):
+	if velocity==Vector2.ZERO:
 		return
-	
-	# 移動量
-	var move_value=velocity*TILE_SIZE
-	
-	# 新しい位置
-	var new_position=position+move_value
-	
-	# 移動できるかチェック
+
+	# 移動量を計算
+	var move_value=	velocity*TILE_SIZE
+
+	# 移動チェック	
 	if test_move(global_transform,move_value):
 		sound_stop.play_sound()
 		return
+
+	# 新しい位置
+	var new_position = position+move_value
+		
+	# 新しい位置をタイルのグリッドに合わせて丸める
+	new_position.x = round(new_position.x / TILE_SIZE) * TILE_SIZE
+	new_position.y = round(new_position.y / TILE_SIZE) * TILE_SIZE
 	
-	# 丸める
-	new_position.x=round(new_position.x/TILE_SIZE)*TILE_SIZE
-	new_position.y=round(new_position.y/TILE_SIZE)*TILE_SIZE
-	
-	# 移動
 	position=new_position
-	sound_go.play_sound()	
+	sound_go.play_sound()
