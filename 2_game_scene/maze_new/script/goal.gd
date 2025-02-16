36a535c7f4a6ec2d82fc369b71e8c3597c7131a6
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-16 20:29:45 +0900

    call main function

--- 2_game_scene/maze_new/script/goal.gd
+++ 2_game_scene/maze_new/script/goal.gd
@@ -14,4 +14,8 @@ func _process(delta: float) -> void:
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 		# playerが接したときにゴール
 	if(body.name=="player"):
+		# 音を鳴らす
 		sound_goal.play_sound()
+		
+		# シーンチェンジの関数呼び出し
+		get_parent().change_scene()
