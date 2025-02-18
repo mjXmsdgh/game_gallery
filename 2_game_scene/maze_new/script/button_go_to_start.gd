6888bdc884a6ba19a63e54f0f4b17a03c5c054c8
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-18 20:19:07 +0900

    add start_scene

---
+++ 2_game_scene/maze_new/script/button_go_to_start.gd
@@ -0,0 +1,16 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	# mainに遷移
+	get_tree().change_scene_to_file("res://2_game_scene/maze_new/scene/start.tscn")
