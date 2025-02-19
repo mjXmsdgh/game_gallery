d3be62ddc32e4c4850cbe52cd8e5663d4272729d
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-19 20:33:38 +0900

    delete export

--- 2_game_scene/maze/script/button_start.gd
+++ 2_game_scene/maze/script/button_start.gd
@@ -1,6 +1,5 @@
 extends Button
 
-@export var main_scene:PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- 2_game_scene/maze/script/main.gd
+++ 2_game_scene/maze/script/main.gd
@@ -1,5 +1,4 @@
 extends Node2D
-@export var end_scene:PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
