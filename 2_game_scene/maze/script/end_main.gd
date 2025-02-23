86ed906faf29857411630ec0672e02e48c02a527
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-23 20:44:38 +0900

    delete button_go_to_start script

--- 2_game_scene/maze/script/end_main.gd
+++ 2_game_scene/maze/script/end_main.gd
@@ -1,11 +1,11 @@
-extends Button
+extends Node2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var time:String=Global.get_time()
 	
-	$"../clear_time".text="Clear Time : "+time
-	
+	$"clear_time".text="Clear Time : "+time
 	pass # Replace with function body.
 
 
@@ -14,6 +14,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_pressed() -> void:
-	# mainに遷移
+func _on_button_go_to_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://2_game_scene/maze/scene/start.tscn")
