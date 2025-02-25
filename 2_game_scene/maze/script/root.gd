fe67e40c317f4140e7d1cda5d4aea1dfcada54e7
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-02-25 20:09:27 +0900

    change  resource method

--- 2_game_scene/maze/script/root.gd
+++ 2_game_scene/maze/script/root.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var start_scene_resource:PackedScene  # start
+@export var main_scene_resource:PackedScene   # main
+@export var end_scene_resource:PackedScene    # end
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +17,10 @@ func _process(delta: float) -> void:
 func start_to_main()->void:
 	
 	# mainシーンをload
-	var main_scene:Node=load("res://2_game_scene/maze/scene/main.tscn").instantiate()
+	var main_scene:Node=main_scene_resource.instantiate()
 	
 	# 子ノードに追加
 	add_child(main_scene)
-	print(main_scene.name)
 	
 	# シグナルを接続
 	$Main.connect("main_to_end",main_to_end)
@@ -28,22 +30,28 @@ func start_to_main()->void:
 
 func main_to_end()->void:
 	# endシーンをload
-	var end_scene:Node=load("res://2_game_scene/maze/scene/end.tscn").instantiate()
+	var end_scene:Node=end_scene_resource.instantiate()
 	
 	# 子ノードに追加
 	add_child(end_scene)
 	
+	# シグナルを接続
 	$End.connect("end_to_start",end_to_main)
 	
+	# mainノードを開放
 	$Main.queue_free()
 
 
 func end_to_main()->void:
 	
-	var start_scene:Node=load("res://2_game_scene/maze/scene/start.tscn").instantiate()
+	# startシーンをload
+	var start_scene:Node=start_scene_resource.instantiate()
 	
+	# 子ノードに追加
 	add_child(start_scene)
 	
+	# シグナルを接続
 	$Start.connect("change_to_main",start_to_main)
 	
+	# endノードを開放
 	$End.queue_free()
