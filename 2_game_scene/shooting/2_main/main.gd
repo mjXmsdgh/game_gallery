89bbd9c9f8f9dd7aea095bf84882842c6a402b2e
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-03-03 20:34:25 +0900

    split function and add type hint

--- 2_game_scene/shooting/2_main/main.gd
+++ 2_game_scene/shooting/2_main/main.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-var time=0
-var score=0
+var time:float=0.0
+var score:int=0
 
-func update_score() -> void:
-
-	# CanvasLayer 内の ScoreLabel のテキストを、現在のスコアに更新する
-	$CanvasLayer/ScoreLabel.text=str(score)
+const ENEMY_SCORE:int = 100 # 敵を倒した時のスコア
+const ONE_SECOND:float = 1.0 # 1秒
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,23 +15,48 @@ func _ready() -> void:
 
 	# enemy ノードのシグナルを接続する
 	connect_enemy_signals()
+	
+	# ゴールのシグナルを接続
+	$Goal_flag.connect("goal_signal",goal_func)
+
+
+func update_score() -> void:
+
+	# CanvasLayer 内の ScoreLabel のテキストを、現在のスコアに更新する
+	$CanvasLayer/ScoreLabel.text=str(score)
 
 
 func connect_enemy_signals() -> void:
 
 	# "enemy" グループに属するすべてのノードを取得する（複数の敵が存在する場合を想定）
-	var enemys=get_tree().get_nodes_in_group("enemy")
+	var enemys:Array[Node]=get_tree().get_nodes_in_group("enemy")
 
 	# 取得した各 enemy ノードに対して処理を行う
 	for enemy in enemys:
 
-		# 各 enemy ノードの "enemy_deleted" シグナルを、このスクリプトの "_on_enemy_destroyed" 関数に接続する
-		enemy.connect("enemy_deleted",_on_enemy_destroyed)
+		# 各 enemy ノードの "enemy_deleted" シグナルを、このスクリプトの "_on_enemy_deleted" 関数に接続する
+		enemy.connect("enemy_deleted",_on_enemy_deleted)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
+	_handle_player_input()
+		
+	# 時間計測：delta を time に加算する（経過時間を記録）
+	time=time+delta
 	
+	# 1 秒経過したか判定
+	if time>=ONE_SECOND:
+
+		# 1秒ごとにプレイヤーが上方向に移動するようにする
+		$player._on_button_up_pressed()
+
+		# time をリセット
+		time=0
+
+
+func _handle_player_input() -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		$player._on_button_up_pressed()
 	elif Input.is_action_just_pressed("ui_down"):
@@ -44,24 +67,11 @@ func _process(delta: float) -> void:
 		$player._on_button_right_pressed()
 	elif Input.is_action_just_pressed("key_space"):
 		$player.shoot()
-		
-	# 時間計測：delta を time に加算する（経過時間を記録）
-	time=time+delta
-	
-	# 1 秒経過したか判定
-	if time>=1.0:
 
-		# 1秒ごとにプレイヤーが上方向に移動するようにする
-		$player._on_button_up_pressed()
-
-		# time をリセット
-		time=0
-
-
-func _on_enemy_destroyed()->void:
+func _on_enemy_deleted()->void:
 
 	# スコア追加
-	add_score(100)
+	add_score(ENEMY_SCORE)
 
 
 func add_score(input_score: int) -> void:
@@ -71,3 +81,6 @@ func add_score(input_score: int) -> void:
 
 	# スコア表示を更新する
 	update_score()
+
+func goal_func() -> void:
+	print("get goal")
