6dc22f044ee74f71a6b7bce99e22597e227d4570
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-03-29 20:30:54 +0900

    add type hint

--- 2_game_scene/min_search/script/world_to_screen.gd
+++ 2_game_scene/min_search/script/world_to_screen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # Axisノードへの参照
-@onready var axis_node=get_node_or_null("../../AxisNode")
+@onready var axis_node: Node2D = get_node_or_null("../../AxisNode") as Node2D
 
 # ワールド座標をスクリーン座標に変換
 func world_to_screen(world_pos: Vector2) -> Vector2:
@@ -17,19 +17,19 @@ func world_to_screen(world_pos: Vector2) -> Vector2:
 		return Vector2.ZERO
 
 	var center: Vector2 = axis_node.get_screen_center()
-	var screen_size = axis_node.get_screen_size()
+	var screen_size: Vector2 = axis_node.get_screen_size()
 	
 	# ここでAxisNodeのx_min,x_max等を取得する必要がある。ただしそれはAxisNodeの責務
-	var x_min = axis_node.get_x_min()
-	var x_max = axis_node.get_x_max()
-	var y_min = axis_node.get_y_min()
-	var y_max = axis_node.get_y_max()
-	var world_width = x_max - x_min
-	var world_height = y_max - y_min
-	var screen_width = screen_size.x
-	var screen_height = screen_size.y
+	var x_min: float = axis_node.get_x_min()
+	var x_max: float = axis_node.get_x_max()
+	var y_min: float = axis_node.get_y_min()
+	var y_max: float = axis_node.get_y_max()
+	var world_width: float = x_max - x_min
+	var world_height: float = y_max - y_min
+	var screen_width: float = screen_size.x
+	var screen_height: float = screen_size.y
 
-	var screen_x = center.x + (world_pos.x - ((x_max + x_min) / 2)) * (screen_width / world_width)
-	var screen_y = center.y - (world_pos.y - ((y_max + y_min) / 2)) * (screen_height / world_height)
+	var screen_x: float = center.x + (world_pos.x - ((x_max + x_min) / 2)) * (screen_width / world_width)
+	var screen_y: float = center.y - (world_pos.y - ((y_max + y_min) / 2)) * (screen_height / world_height)
 
 	return Vector2(screen_x, screen_y)
