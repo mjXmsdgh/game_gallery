c411e1e134c5bfa300e562ad7ae733c43fe1de2f
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-04-01 20:20:30 +0900

    change by gemini

--- 2_game_scene/min_search/script/axis.gd
+++ 2_game_scene/min_search/script/axis.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
+# 軸の描画を担当するノード
 
+# 軸の最大値と最小値
 var x_min: float = -10.0
 var x_max: float = 10.0
 var y_min: float = -10.0
 var y_max: float = 10.0
+
+
+func set_x_range(x_min_val: float, x_max_val: float) -> void:
+	x_min=x_min_val
+	x_max=x_max_val
+
+	queue_redraw()
+
+func set_y_range(y_min_val: float, y_max_val: float) -> void:
+	y_min=y_min_val
+	y_max=y_max_val
+
+	queue_redraw()
+
 
 # 各プロパティのアクセサ
 func get_x_min() -> float:
@@ -19,29 +35,33 @@ func get_y_min() -> float:
 func get_y_max() -> float:
 	return y_max
 
-# 軸の描画を担当するノード
 
 # 画面の幅と高さを取得
 func get_screen_size() -> Vector2:
+	
 	# ビューポート（画面）の矩形領域のサイズを取得します。
 	var screen_width: float = get_viewport_rect().size.x
 	var screen_height: float = get_viewport_rect().size.y
-	# 画面の幅と高さをVector2として返します。
+	
 	return Vector2(screen_width, screen_height)
 
 # 画面の中心の座標を取得
 func get_screen_center() -> Vector2:
+	
 	# 画面のサイズを取得します。
 	var screen_size: Vector2 = get_screen_size()
+	
 	# 画面の中心座標を計算します。
 	var center_position: Vector2 = Vector2(screen_size.x / 2, screen_size.y / 2)
-	# 画面の中心座標をVector2として返します。
+	
 	return center_position
 
 # 軸の描画
 func draw_axis():
+	
 	# 画面の中心座標を取得します。
 	var center: Vector2 = get_screen_center()
+	
 	# 画面のサイズを取得します。
 	var screen_size: Vector2 = get_screen_size()
 
@@ -61,13 +81,15 @@ func draw_axis():
 	# y軸を描画します。色は濃い灰色で、太さは5.0です。
 	draw_line(y_axis_start, y_axis_end, Color.DIM_GRAY,5.0)
 
+
 # ノードがシーンツリーに追加されたときに呼び出されます。
 func _ready():
 	# 描画をキューに入れます。つまり、次のフレームで_draw()が実行されます。
 	queue_redraw()
 
-# 描画処理
-# 描画を行う関数です。queue_redraw()によって呼び出されます。
+
+# 描画処理。queue_redraw()によって呼び出されます。
 func _draw():
+
 	# 軸を描画します。
 	draw_axis()
